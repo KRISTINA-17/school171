ee2906d65d48a315a2bdf0188de280f2aaeacc11
KRISTINA-17 <yushastic@gmail.com>
2017-02-26 11:37:05 +0300

    20170226

---
+++ Пингвины/Project1/main.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+void paint(int n){
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("   _~_    ");
+	}
+	printf("\n");
+
+	for (i = 0; i < n; i++)
+	{
+		printf("  (o o)   ", i);
+	}
+	printf("\n");
+
+	for (i = 0; i < n; i++)
+	{
+		printf(" /  V  \\  ");
+	}
+	printf("\n");
+
+	for (i = 0; i < n; i++)
+	{
+		printf("/(  _  )\\ ");
+	}
+	printf("\n");
+
+	for (i = 0; i < n; i++)
+	{
+		printf("  ^^ ^^   ");
+	}
+	printf("\n\n");
+}
+int main(){
+	int n, i;
+	scanf("%d", &n);
+	for (int i = 0; i < n / 7; i++)
+	{
+		paint(7);
+	}
+	paint(n % 7);
+	
+
+	return 0;
+}
